3d8be50b39abbcc2454620ce70af7a4d1b324873
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2013-02-22 16:38:25 +0900

    Make a server print more useful informations.

--- server.rb
+++ server.rb
@@ -11,16 +11,18 @@ EventMachine.run {
   @channel = EventMachine::Channel.new
 
   EventMachine::WebSocket.start(host: "0.0.0.0", port: 8888) do |ws|
-    ws.onopen {
+    ws.onopen {|handshake|
       sid = @channel.subscribe {|msg|
         ws.send(msg)
       }
+      $stderr.puts("#{sid} connected to '#{handshake.path}'.")
       ws.onmessage {|msg|
         @channel.push(msg)
-        p msg
+        $stderr.puts("#{sid} pushed a message '#{msg}'.")
       }
       ws.onclose {
         @channel.unsubscribe(sid)
+        $stderr.puts("#{sid} disconnected.")
       }
     }
   end
