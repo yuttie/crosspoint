843de3531ea5f420dfa0b00026f1bb226a0649d1
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2013-02-22 16:26:11 +0900

    Rewrite the WebSocket server without threads.
    Learned from this example:
    https://github.com/igrigorik/em-websocket/blob/master/examples/multicast.rb

--- server.rb
+++ server.rb
@@ -6,33 +6,22 @@ if RUBY_VERSION >= '1.9'
 end
 
 require 'em-websocket'
-require 'thread'
 
-ts = []
-EventMachine::WebSocket.start(host: "0.0.0.0", port: 8888) do |ws|
-  thread = nil
-  ws.onmessage {|msg|
-    ts.each {|t| t[:queue].push(msg) }
-    p msg
-  }
-  ws.onopen {
-    thread = Thread.new {
-      this = Thread.current
-      this[:queue] = Queue.new
-      while true
-        msg = this[:queue].pop
-        if (msg == "end")
-          break
-        else
-          ws.send(msg)
-        end
-      end
+EventMachine.run {
+  @channel = EventMachine::Channel.new
+
+  EventMachine::WebSocket.start(host: "0.0.0.0", port: 8888) do |ws|
+    ws.onopen {
+      sid = @channel.subscribe {|msg|
+        ws.send(msg)
+      }
+      ws.onmessage {|msg|
+        @channel.push(msg)
+        p msg
+      }
+      ws.onclose {
+        @channel.unsubscribe(sid)
+      }
     }
-    ts << thread
-    p ts
-  }
-  ws.onclose {
-    ts.delete(thread)
-    p ts
-  }
-end
+  end
+}
