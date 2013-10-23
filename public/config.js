2606c9abe44843658fae97cdea1cd4b33d907994
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2013-10-23 20:59:19 +0900

    Add initial columns for example usage

--- public/config.js
+++ public/config.js
@@ -8,6 +8,20 @@ Xpt.initialize({
         , out_map:    function(p) { return p; }
         , entry_placeholder: null
         },
+        { title:   "Q&A (ハッシュタグ#QA)"
+        , in_filter:  function(p) { return p.content.match(/#QA/i); }
+        , out_filter: function(p) { return true; }
+        , in_map:     function(p) { return p; }
+        , out_map:    function(p) { p.content += "#QA"; return p; }
+        , entry_placeholder: "質問する... / 回答する..."
+        },
+        { title:   "TA"
+        , in_filter:  function(p) { return p.content.match(/#TA/i); }
+        , out_filter: function(p) { return true; }
+        , in_map:     function(p) { p.content = p.content.replace(/#TA/ig, ""); return p; }
+        , out_map:    function(p) { p.content += "#TA"; return p; }
+        , entry_placeholder: "質問する... / 回答する..."
+        },
         { title:   "あなたのグループ内のメッセージ"
         , in_filter:  function(p) { return p.content.match(/#GROUP-ONLY/i) && Xpt.readFromStorage('group_id') === p.user.group_id; }
         , out_filter: function(p) { return true; }
