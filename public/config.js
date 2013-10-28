dab763ad0a4075f76213016d1a8b0cc6c39a7f6d
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2013-10-28 13:07:33 +0900

    Remove the group-only column

--- public/config.js
+++ public/config.js
@@ -2,7 +2,7 @@ Xpt.initialize({
     app_title: "Crosspoint",
     init_columns: [
         { title:   "全体のメッセージ"
-        , in_filter:  function(p) { return !p.content.match(/#GROUP-ONLY/i); }
+        , in_filter:  function(p) { return true; }
         , out_filter: function(p) { return true; }
         , in_map:     function(p) { return p; }
         , out_map:    function(p) { return p; }
@@ -21,13 +21,6 @@ Xpt.initialize({
         , in_map:     function(p) { p.content = p.content.replace(/##TA/ig, ""); return p; }
         , out_map:    function(p) { p.content += "##TA"; return p; }
         , entry_placeholder: "TAのみ書き込めます"
-        },
-        { title:   "あなたのグループ内のメッセージ"
-        , in_filter:  function(p) { return p.content.match(/#GROUP-ONLY/i) && Xpt.readFromStorage('group_id') === p.user.group_id; }
-        , out_filter: function(p) { return true; }
-        , in_map:     function(p) { p.content = p.content.replace(/#GROUP-ONLY/ig, ""); return p; }
-        , out_map:    function(p) { p.content += "#GROUP-ONLY"; return p; }
-        , entry_placeholder: null
         }
     ]
 });
